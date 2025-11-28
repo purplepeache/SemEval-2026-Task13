64748fd4c88a33924d9e6c56cff6ab54419b56ad
root <ren>
2025-11-28 17:28:03 +0000

    .

---
+++ reqs.sh
@@ -0,0 +1,4 @@
+pip install transformers magika scikit-learn Pygments matplotlib seaborn pyarrow fastparquet sentence-transformers hf_transfer kaggle
+pip install spacy
+
+python spacy download en_core_web_lg
